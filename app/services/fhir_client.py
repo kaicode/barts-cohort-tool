11be260c205f2dbd60f69362f0494d032d9360c5
Kai Kewley <kaikewley@gmail.com>
2025-09-25 17:24:01 +0100

    Increase terminology server request timeout to 30s

--- app/services/fhir_client.py
+++ app/services/fhir_client.py
@@ -35,12 +35,12 @@ class FHIRClient:
 
     def search_snomed(self, ecl: str, term: str, count: int = 20):
         url = f"{settings.fhir_api_url}/ValueSet/$expand?url=http://snomed.info/sct?fhir_vs=ecl/{ecl}&filter={term}"
-        response = self.session.get(url, headers=self.get_headers(), timeout=10)
+        response = self.session.get(url, headers=self.get_headers(), timeout=30)
         return response.json()
 
     def map_snomed_to_icd10(self, snomed_code):
         source_system = "http://snomed.info/sct"
         target_system = "http://hl7.org/fhir/sid/icd-10"
         url = f"{settings.fhir_api_url}/ConceptMap/$translate?code={snomed_code}&system={source_system}&targetsystem={target_system}"
-        response = self.session.get(url, headers=self.get_headers(), timeout=10)
+        response = self.session.get(url, headers=self.get_headers(), timeout=30)
         return response.json()
